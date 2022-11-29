afd604e26c24dd87a704d836b33893093a1ecebb
William Wheeler <williamwheelertx@gmail.com>
2022-11-29 17:39:35 -0600

    add stripe divider option in About

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,12 @@ function About() {
             <div className="aboutHeader">
                 <h1>About</h1>
                 <hr></hr>
-            </div>
-            <div className="stripeContainer">
-                <div className="stripe orange"></div>
-                <div className="stripe redOrange"></div>
-                <div className="stripe navy"></div>
-                <div className="stripe darkBlue"></div>
+                {/* <div className="stripeContainer line">
+                    <div className="stripe orange"></div>
+                    <div className="stripe redOrange"></div>
+                    <div className="stripe navy"></div>
+                    <div className="stripe darkBlue"></div>
+                </div>     */}
             </div>
         </div>
     );
