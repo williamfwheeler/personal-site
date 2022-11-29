afbb054c85670aa831af73651bd5f7227c561699
William Wheeler <williamwheelertx@gmail.com>
2022-11-29 17:09:46 -0600

    add stripe to HERO

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,62 @@ function Hero() {
     return (
         <div className="hero">
             <h1>Hero</h1>
+            <div className="illustrationContainer">
+
+                <svg>
+                    {/* <circle className="circle1" r="50" cx="100" cy="80"></circle>
+                    <circle className="circle2" r="50" cx="100" cy="80"></circle>
+           
+
+                    <path d="M 40 10 h 90 v 80 z" fill="transparent"/> */}
+
+                    <path className="outer3" 
+                    d="M -80 310 h 410 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    <path className="outer3" 
+                    d="M 0 490 h 480 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                     <path className="outer2" 
+                    d="M -60 350 h 410 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    <path className="outer2" 
+                    d="M 0 450 h 450 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    <path className="outer1" 
+                    d="M -30 380 h 410 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    <path className="outer1" 
+                    d="M 0 425 h 420 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    <path className="main" 
+                    d="M 0 400 h 400 s 150 00, 150 -150
+                    s 150 -150 , 150 -150
+                    h 1300" 
+                    fill="transparent"/>
+
+                    
+                </svg>
+
+
+            </div>
 
         </div>
     );
