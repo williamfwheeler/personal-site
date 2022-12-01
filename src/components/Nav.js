d00b59d6609f1c6ff68b7fa9cd52b4cfd5ee4b7e
William Wheeler <williamwheelertx@gmail.com>
2022-12-01 10:06:29 -0600

    add scroll button f(x) to floppys and gear

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,19 @@ import gear from '../assets/gear_image.png';
 
 function Nav() {
 
+    const handleHome = () => {
+        window.scrollTo({
+            behavior: 'smooth',
+            top:0
+            });
+    }
+
     return (
         <div className="navBar">
             <div className="siteName">WW Workshop</div>
-            <div className="mainLogo"><img src={gear} alt="spinning gear"></img></div>
+            <button class="homeButton" onClick={handleHome}>
+                <div className="mainLogo"><img src={gear} alt="spinning gear"></img></div>
+            </button>
             <div className="links">
                 <div>About</div>
                 <div>Skills</div>
