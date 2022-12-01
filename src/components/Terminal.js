2319659d02b0cc96ebcd35724a3b0cebc652c116
William Wheeler <williamwheelertx@gmail.com>
2022-11-30 23:38:52 -0600

    add initial Terminal structure

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -4,15 +4,37 @@ function Terminal() {
     return (
       <div className="terminal">
         <div className="terminalHeader">
-                <h1>Terminal</h1>
-                {/* <hr></hr> */}
-                <div className="stripeContainer line">
-                    <div className="stripe orange"></div>
-                    <div className="stripe redOrange"></div>
-                    <div className="stripe navy"></div>
-                    <div className="stripe darkBlue"></div>
-                </div>    
+          <h1>Terminal</h1>
+          <hr></hr>
+          {/* <div className="stripeContainer line">
+          <div className="stripe darkBlue"></div>
+              <div className="stripe orange"></div>
+              <div className="stripe redOrange"></div>
+              <div className="stripe navy"></div>
+          </div>     */}
+        </div>
+
+
+
+        <div className='terminalContainer'>
+          <div className='terminalScreen'>
+            <div className='screenShell'>
+              <div className='screen'></div>
             </div>
+          </div>
+          <div className='terminalHardware'>
+            <div className='mug'></div>
+            <div className='diskDrive'>
+              <div className='driveInset'>
+                <div></div>
+                <div className="middleDrive">
+                  <div className='floppyInsert'></div>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
