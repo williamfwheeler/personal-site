8d4ea9059131fa811f4c8a0a5f70998fd186b79a
William Wheeler <williamwheelertx@gmail.com>
2022-12-04 16:23:41 -0600

    fix stripe illustration, convert to standalone svg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './hero.css';
+import stripeIllustration from '../assets/stripeIllustration.svg';
 import Floppy from "./Floppy";
 
 function Hero({floppyClick}) {
@@ -10,53 +11,7 @@ function Hero({floppyClick}) {
             <p>/ / / / Web Developer</p>
             </h1>
             <div className="illustrationContainer">
-                <svg className="stripeIllustration"
-                viewBox="180 -00 1300 480"
-                >
-                    <path className="outer3" 
-                    d="M -90 340 h 410 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/>
-
-                    {/* <path className="outer3" 
-                    d="M 0 510 h 480 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/> */}
-
-                     <path className="outer2" 
-                    d="M -60 370 h 410 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/>
-
-                    {/* <path className="outer2" 
-                    d="M 0 470 h 450 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/> */}
-
-                    <path className="outer1" 
-                    d="M -25 400 h 410 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/>
-
-                    {/* <path className="outer1" 
-                    d="M 0 445 h 420 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/> */}
-
-                    <path className="main" 
-                    d="M 0 440 h 410 s 150 00, 150 -150
-                    s 150 -150 , 150 -150
-                    h 1300" 
-                    fill="transparent"/>
-
-                    {/* <text x="670" y="35">/ / / / Web Developer</text> */}
-                </svg>
+                {/* <img src={stripeIllustration}></img> */}
             </div>
             <div className="floppyContainer">
                 <Floppy floppyName={"floppy1"} labelInfo={"ABOUT"} xPercent={"24%"} optionClick={floppyClick}/>
