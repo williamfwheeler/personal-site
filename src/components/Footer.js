6178a83ab27357f1012446974a1ef13a69b5f703
William Wheeler <williamwheelertx@gmail.com>
2022-12-13 13:14:27 -0600

    Add footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import gitHub from '../assets/gitHubFooter.svg';
+import linkedIn from '../assets/linkedInFooter.svg';
+import './footer.css';
+// import bracket from '../assets/bracketFooter.svg';
 
 function Footer() {
 
     return (
-        <h1>Footer</h1>
+        <div className="footer">
+            <div className="footerIcon">
+                <a href="https://www.linkedin.com/in/william-wheeler-ww/">
+                    <img src={linkedIn} alt="LinkedIn icon" />
+                </a>
+            </div>
+            <div className="footerIcon">
+                <a href="https://github.com/williamfwheeler">
+                    <img src={gitHub} alt="gitHub icon" />
+                </a>
+            </div>
+        </div>
     );
 }
 
