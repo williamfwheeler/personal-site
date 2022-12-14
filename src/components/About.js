d2232c3469da6c1fddc5094bfbb74dcaceb3ab17
William Wheeler <williamwheelertx@gmail.com>
2022-12-14 11:41:24 -0600

    Update about page with new copy and formatting

--- src/components/About.js
+++ src/components/About.js
@@ -18,21 +18,15 @@ function About() {
                 </div>     */}
             </div>
             <div className="aboutContent">
-                {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet condimentum augue, at luctus turpis porta at. Nullam dapibus eget nulla in vestibulum. </p>
-                <br></br>
-                <p>Suspendisse potenti. Morbi diam neque, imperdiet ut luctus aliquet, elementum quis elit. In eget vulputate velit, sed ullamcorper velit. Sed sit amet sagittis urna. Cras non congue lorem. Nullam aliquet elit quis augue mattis iaculis. Nam ut bibendum magna. Quisque sed elit urna. Sed eget aliquam ex.</p> */}
 
-                <p>William Wheeler is a Fort Worth-based web developer. With a passion for both coding and problem-solving, William has a unique skill set that allows him to excel at creating user-friendly websites. </p>
+                <p>William Wheeler is a Fort Worth-based web developer. With a passion for both coding and problem-solving, William uses his background in data analytics and music to create websites that are functional, fun, and visually appealing.  </p>
                 <br></br>    
-                <p>Prior to becoming a web developer, he spent several years working for financial service companies, where he honed his ability to analyze large sets of data and identify patterns and trends. Today, he uses his problem-solving expertise to create websites that are both functional and visually appealing. </p>
-                <br></br>
-                <p>In his free time, William enjoys making music and probably reading about your grandpa's favorite topics.</p>
+                <p>In his free time, William enjoys making music and reading about the subjects your grandpa probably likes (boats,plants,history). </p>
 
-                <p className="mobile pageOne">William Wheeler is a Fort Worth-based web developer. With a passion for both coding and problem-solving, William has a unique skill set that allows him to excel at creating user-friendly websites. </p>
+                <p className="mobile pageOne">William Wheeler is a Fort Worth-based web developer. With a passion for both coding and problem-solving, William uses his background in data analytics and music to create websites that are functional, fun, and visually appealing.  </p>
                 <br className="mobile pageOne"></br>    
-                <p className="mobile ">Prior to becoming a web developer, he spent several years working for financial service companies, where he honed his ability to analyze large sets of data and identify patterns and trends. Today, he uses his problem-solving expertise to create websites that are both functional and visually appealing. </p>
-                <br className="mobile "></br>
-                <p className="mobile ">In his free time, William enjoys making music and probably reading about your grandpa's favorite topics.</p>
+                <p className="mobile ">In his free time, William enjoys making music and reading about the subjects your grandpa probably likes (boats,plants,history). </p>
+                
             </div>
         </div>
     );
